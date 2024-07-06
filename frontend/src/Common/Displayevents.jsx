f8e60c7bf38c488463886a965d300a3f3df3a3c4
Surya-viswanath <121759302+Surya-viswanath@users.noreply.github.com>
2024-07-06 16:33:40 +0530

    Update Displayevents.jsx

--- frontend/src/Common/Displayevents.jsx
+++ frontend/src/Common/Displayevents.jsx
@@ -1,5 +1,4 @@
 
-
 // import React, { useEffect, useState } from 'react';
 // import axios from 'axios';
 // // import Lottie from "lottie-react";
@@ -7,9 +6,12 @@
 // import { TbCategory2 } from "react-icons/tb";
 // // import loadingAnimation from "../../assets/animation/animation.json";
 // import Eventcard from './Eventcard';
-// import Container from './Container';
-// import './Event.css';
+
+// // import './Event.css';
 // import Eventbanner from './Eventbanner';
+// import { Card, Col,    Container,    Row } from 'react-bootstrap';
+// import { Link } from 'react-router-dom';
+// import { BsDiagram3, BsTwitterX } from "react-icons/bs";
 
 // function Displayevents() {
 //     const [searchTerm, setSearchTerm] = useState("");
@@ -17,6 +19,12 @@
 //     const [filteredEvents, setFilteredEvents] = useState([]);
 //     const [isLoading, setIsLoading] = useState(true);
 //     const [error, setError] = useState(null);
+    
+
+//     const formatDate = (dateString) => {
+//         const options = { year: 'numeric', month: 'long', day: 'numeric' };
+//         return new Date(dateString).toLocaleDateString(undefined, options);
+//     };
 
 //     useEffect(() => {
 //         const fetchEvents = async () => {
@@ -64,18 +72,25 @@
 //     }
 
 //     return (
-//         <div>
-//             <Eventbanner />
-//             <Container>
-//             {/* <div className="max-w-7xl mx-auto px-4 md:px-8 lg:px-12"> */}
+//         <div style={{margin:'0%'}}>
+//             {/* <Eventbanner /> */}
+//  <Container  style={{ backgroundColor:'black' ,padding:'5% 0%',backgroundSize:'cover'}}>
+//       <Row>
+//       <Col>
+//         <h2 className="text-5xl text-white font-bold">Events</h2>
+//         <Link to='/' style={{color:'#9C0C0D'}}>Home</Link>
+//         {/* <Link to='/events' style={{color:'#9C0C0D'}}>Events</Link> */}
+//         </Col>
+//       </Row>
+//     </Container>
+
+//             <Container >
 //             <h2 className="text-3xl md:text-5xl font-bold text-center text-secondary mt-10 uppercase">
 //                 All Events
 //             </h2>
-//             {/* <div className="grid grid-cols-1 md:grid-cols-3 my-10 md:my-20 gap-6 " > */}
-//             <div className="custom-grid" >
-//                 {/* <div className="md:col-span-1 "> */}
-//                 <div className="md:col-span-1">
-//                     {/* Search */}
+//             <Row style={{marginTop:'5%'}}>
+//             <Col sm={4}>
+                
 //                     <form
 //                             onSubmit={handleSearch}
 //                             className="flex bg-gray-50 p-5 shadow-md rounded-md"
@@ -96,11 +111,9 @@
 //                                 <FaSearch />
 //                             </button>
 //                         </form>
-
-//                     {/* category */}
 //                     <div className="bg-gray-50 shadow-md rounded-md " style={{alignItems:'left'}}>
 //                         <div className=""  >
-//                             <span className="inline-flex   text-white text-xl font-bold p-3 rounded-r-xl"  style={{backgroundColor:'#9C0C0D',marginLeft:'-63%'}}>
+//                             <span className="inline-flex   text-white text-xl font-bold p-3 rounded-r-xl"  style={{backgroundColor:'#9C0C0D',marginLeft:'-57%'}}>
 //                                 <TbCategory2 /> Category
 //                             </span>
 //                         </div>
@@ -139,24 +152,47 @@
 //                     </div>
 
 
-//                 </div>
-
-//                 {/* cards */}
-//                 <div className="md:col-span-2 grid grid-cols-1 sm:grid-cols-2 gap-6" style={{display:'grid',gridTemplateColumns:'repeat(2,1fr)'}}>
-//                     {filteredEvents.map((event) => (
-//                         <Eventcard key={event._id} event={event} />
-//                     ))}
-//                 </div>
-             
-//             </div>
+                
+//             </Col>
+                
+//                 <Col sm={12} md={8}>
+//                         <Row>
+//                             {filteredEvents.map((event) => (
+//                                 <Col xs={12} sm={6} md={6} lg={6} className="mb-3" key={event._id}>
+//                                     <Card style={{ width: '100%' }}>
+//                                         <Card.Img variant="top" src={event.image} style={{ height: '150px' }} />
+//                                         <Card.Body>
+//                                             <Card.Title>{event.title}</Card.Title>
+//                                             <Card.Text>
+//                                                 <div style={{ display: 'flex', alignItems: 'center' }}>
+//                                                     <span style={{ color: '#9C0C0D', paddingRight: '3%' }}>
+//                                                         <BsDiagram3 />
+//                                                     </span>
+//                                                     {event.seat} Seat
+                                               
+                                               
+//                                                 <p style={{paddingLeft:'5%'}}>{formatDate(event.date)}</p>
+//                                                 </div>
+//                                                 <p>{event.location}</p>
+//                                                 <Link to={`/event-details/${event._id}`} className="font-semibold underline" style={{ color: '#9C0C0D' }}>
+//                                                     Event Details
+//                                                 </Link>
+//                                             </Card.Text>
+//                                         </Card.Body>
+//                                     </Card>
+//                                 </Col>
+//                             ))}
+//                         </Row>
+//                     </Col>
+           
+//             </Row>
 //             </Container>
-//             {/* </div> */}
+           
 //         </div>
 //     );
 // }
 
 // export default Displayevents;
-
 
 
 
@@ -325,16 +361,16 @@ function Displayevents() {
                                         <Card.Body>
                                             <Card.Title>{event.title}</Card.Title>
                                             <Card.Text>
-                                                <div style={{ display: 'flex', alignItems: 'center' }}>
+                                               
+                                                <p style={{ display: 'flex'}}>
                                                     <span style={{ color: '#9C0C0D', paddingRight: '3%' }}>
                                                         <BsDiagram3 />
                                                     </span>
-                                                    {event.seat} Seat
-                                               
-                                               
-                                                <p style={{paddingLeft:'5%'}}>{formatDate(event.date)}</p>
-                                                </div>
-                                                <p>{event.location}</p>
+                                                    {event.seat} Seat 
+                                                    <h6 style={{paddingLeft:'10%'}}>{formatDate(event.date)}</h6>
+                                                    </p>
+                                                
+                                                <p style={{textAlign:'center'}}>{event.location}</p>
                                                 <Link to={`/event-details/${event._id}`} className="font-semibold underline" style={{ color: '#9C0C0D' }}>
                                                     Event Details
                                                 </Link>
